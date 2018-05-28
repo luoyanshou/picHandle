35bc1ef07ee85e1e5f7e638974460d03f2e32b46
shoushou <shoushoustar@sina.com>
2018-05-28 15:28:23 +0800

    项目结构部署

---
+++ conf/const.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DS", DIRECTORY_SEPARATOR);
+defined("WEB_DIR")    or define("WEB_DIR", __DIR__);
+defined("SMARTY_DIR") or define("SMARTY_DIR", WEB_DIR.DS."vendor".DS."smarty_3131");
+
--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+
+require_once __DIR__.DIRECTORY_SEPARATOR."conf".DIRECTORY_SEPARATOR."const.php";
